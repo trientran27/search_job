eeffcc58966f03728dee320a4844a8c1c61fa6cd
trientran27 <115386535+trientran27@users.noreply.github.com>
2024-07-08 15:18:43 +0700

    commit job

--- searchJob/src/main/java/com/example/searchJob/SearchJobApplication.java
+++ searchJob/src/main/java/com/example/searchJob/SearchJobApplication.java
@@ -2,12 +2,30 @@ package com.example.searchJob;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 
 @SpringBootApplication
 public class SearchJobApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SearchJobApplication.class, args);
+	}
+	
+
+	@Bean
+	public Cloudinary cloudinary() {
+		Cloudinary c = new Cloudinary(ObjectUtils.asMap(
+				
+				"cloud_name", "dulramupj",
+				"api_key", "854114746725916",
+				"api_secret", "rzpp70b3NRu8KCT0LCieJYXeAJ0",
+				"secure",true
+				));
+		
+		return c;
 	}
 
 }
